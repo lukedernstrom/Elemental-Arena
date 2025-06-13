47b0aabea01773335076a20b8018fe6a4f2a6f72
lukedernstrom <97061481+lukedernstrom@users.noreply.github.com>
2025-06-13 15:35:43 -0700

    Removed "DontDestroyOnLoad in GameManager. Fixes crashing issue

--- My project/Assets/_GAME_/Scripts/GameManager.cs
+++ My project/Assets/_GAME_/Scripts/GameManager.cs
@@ -28,7 +28,7 @@ public class GameManager : MonoBehaviour
             return;
         }
         Instance = this;
-        DontDestroyOnLoad(gameObject);
+        // DontDestroyOnLoad(gameObject);
     }
 
     private void Start()
